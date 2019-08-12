14c82f09de750b9dff46070c431b70301c99b0dc
Sergei Gladkikh <l1fepro@yandex.ru>
2019-08-12 21:04:46 +0300

    PassData

---
+++ PassDataProject/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  PassDataProject
+//
+//  Created by L1fePro on 12.08.2019.
+//  Copyright © 2019 L1fePro. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func sendPressed (button: UIButton) {
+        
+    }
+    
+}
--- PassDataProject/ViewController.swift
+++ PassDataProject/ViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBAction func sendPressed(button: UIButton) {
+        
     }
+        
+    
+    
+
 
 
 }
