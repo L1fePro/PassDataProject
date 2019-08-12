b1f8cac316395d31b4a258c32d1413e3ee0989fe
Sergei Gladkikh <l1fepro@yandex.ru>
2019-08-12 19:28:03 +0300

    Initial Commit

---
+++ PassDataProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassDataProject
+//
+//  Created by L1fePro on 12.08.2019.
+//  Copyright © 2019 L1fePro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
