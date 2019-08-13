7843036378c550f92d905bd20af57b2ba3cdec99
Sergei Gladkikh <l1fepro@yandex.ru>
2019-08-13 19:55:14 +0300

    '

--- PassDataProject/SecondViewController.swift
+++ PassDataProject/SecondViewController.swift
@@ -10,10 +10,20 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    
+    var login: String!
+    
     @IBOutlet weak var label: UILabel!
     
     @IBAction func sendPressed (button: UIButton) {
+        performSegue(withIdentifier: "uwindSegue", sender: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        guard let login = login else {return}
+     label.text = "Hello, \(login)"
     }
     
 }
--- PassDataProject/ViewController.swift
+++ PassDataProject/ViewController.swift
@@ -13,14 +13,32 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var returnLabel: UILabel!
+
+
     @IBAction func sendPressed(button: UIButton) {
-        
+        performSegue(withIdentifier: "detailSegue", sender: nil)
     }
+    
+    
+    
+    
+    @IBAction func unwindToMainScreen (segue: UIStoryboardSegue) {
+        guard segue.identifier == "uwindSegue" else {return}
         
+       guard let svc = segue.source as? SecondViewController else {return}
+        self.returnLabel.text = svc.label.text
+    }
     
+        
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
     
-
-
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let dvc = segue.destination as? SecondViewController else {return}
+        dvc.login = loginTextField.text
+    }
+    
 }
 
